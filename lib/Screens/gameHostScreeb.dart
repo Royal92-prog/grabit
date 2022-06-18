c3d4c77ece60d2850aa471b62fd0a4be7344c7ee
Royal92-prog <royal@campus.technion.ac.il>
2022-06-18 17:56:25 +0300

    adding hostGame screen

---
+++ lib/Screens/gameHostScreeb.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+
+import '../main.dart';
+import 'package:tuple_dart/tuple.dart';
+
+
+class GameHost extends StatefulWidget {
+
+  @override
+  State<GameHost> createState() => GameHostStates();
+
+}
+class GameHostStates extends State<GameHost>{
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    
+  }
+}
